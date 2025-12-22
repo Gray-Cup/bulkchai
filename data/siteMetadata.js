babf5472f8c65e4679da4ef29e1d48f8321c096f
BMO <meow@nermalcat69.dev>
2025-12-22 19:13:32 +0530

    add og image

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteUrl: 'https://bulkchai.vercel.app',
   siteRepo: 'https://github.com/timlrx/bulkchai',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/og.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'bulk@graycup.in',
   github: 'https://github.com/gray-cup',
